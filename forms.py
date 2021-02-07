15e3cd202a7f1f677f837b7ba5c8db4833c496dd
Don H <hurstdon1@gmail.com>
2021-02-07 13:56:14 -0800

    Added to forms, gpu, and static files for adding to the DB

--- forms.py
+++ forms.py
@@ -1,8 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, BooleanField, TextAreaField, SubmitField
+from wtforms import TextField, BooleanField, TextAreaField, SubmitField, IntegerField
 class ContactForm(FlaskForm):
     name = TextField("Name")
     email = TextField("Email")
     subject = TextField("Subject")
     message = TextAreaField("Message")
     submit = SubmitField("Send")
+
+class gpuForm(FlaskForm):
+	priceId = IntegerField("Price Id Number")
+	memoryType = TextField("Memory Type")
+	numberOfCudaCores = IntegerField("Number Of Cuda Cores")
+	chipsetId = IntegerField("Chipset Id Number")
+	submit = SubmitField("Submit")
+
+
+class brandForm(FlaskForm):
+	brandName = TextField("Brand Name")
+	productSeries = TextField("Product Series")
+	model = TextField("Model Name")
+	submit = SubmitField("Submit")
+
+class chipsetForm(FlaskForm):
+	chipsetManufacturer=TextField("Chipset Manufacturer")
+	graphicsCoprocessor = TextField("Graphics Coprocessor")
+	submit = SubmitField("Submit")
+
+class pricingForm(FlaskForm):
+	pass
+
+class benchmarkForm(FlaskForm):
+	pass
--- gpu.py
+++ gpu.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from forms import ContactForm
+from forms import ContactForm, gpuForm, brandForm, chipsetForm
 from flask import request
 from flask_wtf import FlaskForm
 from wtforms import TextField, BooleanField, TextAreaField, SubmitField
@@ -27,6 +27,66 @@ def about():
 @app.route("/comparisons")
 def comparisons():
 	return render_template('comparisons.html', title='Comparisons')
+
+@app.route("/add")
+def add():
+	return render_template('add.html', title='Add')
+
+@app.route("/add_gpu", methods=["GET","POST"])
+def add_gpu():
+
+	form = gpuForm()
+
+	if request.method == 'POST':
+		priceId = request.form["priceIdNumber"]
+		memoryType = request.form["memoryType"]
+		numberOfCudaCores = request.form["numberOfCudaCores"]
+		chipsetId = request.form["chipsetId"]
+		res = pd.DataFrame({'priceID':priceID,'memoryType':memoryType, 'numberOfCudaCores':numberOfCudaCores, 'chipsetId':chipsetId}, index=[0])
+		res.to_csv('./add_gpu.csv')
+		print("The data are saved")
+
+	else:
+		return render_template("add_gpu.html", form=form, title="Add a gpu")
+
+@app.route("/add_brand", methods=["GET","POST"])
+def add_brand():
+
+	form = brandForm()
+
+	if request.method == 'POST':
+		brandName = request.form["brandName"]
+		productSeries = request.form["productSeries"]
+		model = request.form["model"]
+		res = pd.DataFrame({'brandName':brandName, 'productSeries':productSeries, 'model':model}, index=[0])
+		res.to_csv('./add_brand.csv')
+		print("The data are saved")
+
+	else:
+		return render_template("add_brand.html", form=form, title="Add a brand")
+
+@app.route("/add_chipset", methods=["GET","POST"])
+def add_chipset():
+
+	form = chipsetForm()
+
+	if request.method == 'POST':
+		chipsetManufacturer = request.form["chipsetManufacturer"]
+		graphicsCoprocessor = request.form["graphicsCoprocessor"]
+		res = pd.DataFrame({'chipsetManufacturer':chipsetManufacturer, 'graphicsCoprocessor':graphicsCoprocessor}, index=[0])
+		res.to_csv('./add_chipset.csv')
+		print("The data are saved")
+
+	else:
+		return render_template("add_chipset.html", form=form, title="Add a chipset")
+
+@app.route("/add_pricing", methods=["GET","POST"])
+def add_pricing():
+	return render_template("add_pricing.html", title="Add Pricing")
+
+@app.route("/add_benchmarks", methods=["GET","POST"])
+def add_benchmarks():
+	return render_template("add_benchmarks.html", title="Add Benchmarks")
 
 @app.route("/contact", methods=["GET","POST"])
 def contact():
